b61ce4b24b176821436043dedac361b0e19293a4
Cesar Anacoreta <cesaranacoreta29@gmail.com>
2023-01-27 22:59:06 -0600

    Modificaion de Variables.js

--- CursoDePreparacionHenry/Variables.js
+++ CursoDePreparacionHenry/Variables.js
@@ -1,1 +1,18 @@
-var cajon;
+var cajon1='Bufanda verde';
+console.log (cajon1);
+// Strings son palabras que se van a guardar en la declaracion de una variable
+var Palabra = 'Textos'
+
+// Numeros
+
+var NumeroEntero=17;
+console.log(NumeroEntero);
+
+var NuemroNegativo=-5;
+var NumeroDecimal=12.5;
+
+// Booleanod
+
+var verdad=true;
+var falso=false;
+
